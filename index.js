f999e901b2fa41e2bbd766fa5853d56dd2c608cd
DuckyBlender <alan@alanklas.com>
2022-02-28 20:09:01 +0100

    Add easier event and handler method

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js")
 require("dotenv").config()
 
-const generateImage = require("./generateImage")
+// const generateImage = require("./generateImage")
 
 const client = new Discord.Client({
     intents: [
@@ -10,24 +10,43 @@ const client = new Discord.Client({
         "GUILD_MEMBERS"
     ]
 })
-client.on("ready", () => {
-    console.log(`Logged in as ${client.user.tag}`)
-})
 
-client.on("messageCreate", (message) => {
-    if (message.content == "Hello World!"){
-        message.reply("Hello World!")
-    }
-})
+let bot = {
+    client,
+    prefix: "n.",
+    owners: ["445928169350889472"]
+}
 
-const welcomeChannelId = "947635791276961813"
+client.commands = new Discord.Collection()
+client.events = new Discord.Collection()
 
-client.on("guildMemberAdd", async (member) => {
-    const img = await generateImage(member)
-    member.guild.channels.cache.get(welcomeChannelId).send({
-        content: `<@${member.id}> Welcome to the server!`,
-        files: [img]
-    })
-})
+client.loadEvents = (bot, reload) => require("./handlers/events")(bot, reload)
+client.loadCommands = (bot, reload) => require("./handlers/commands")(bot, reload)
+
+client.loadEvents(bot, false)
+client.loadCommands(bot, false)
+
+module.exports = bot
+
+
+// client.on("ready", () => {
+//     console.log(`Logged in as ${client.user.tag}`)
+// })
+// 
+// client.on("messageCreate", (message) => {
+//     if (message.content == "Hello World!"){
+//         message.reply("Hello World!")
+//     }
+// })
+// 
+// const welcomeChannelId = "947635791276961813"
+// 
+// client.on("guildMemberAdd", async (member) => {
+//     const img = await generateImage(member)
+//     member.guild.channels.cache.get(welcomeChannelId).send({
+//         content: `<@${member.id}> Welcome to the server!`,
+//         files: [img]
+//     })
+// })
 
 client.login(process.env.TOKEN)
