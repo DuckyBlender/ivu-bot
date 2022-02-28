28ffb1f44a2713d88c551d815bcfc5d0d388792a
DuckyBlender <alan@alanklas.com>
2022-02-28 01:30:00 +0100

    Add welcome message with image

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const Discord = require("discord.js")
 require("dotenv").config()
+
+const generateImage = require("./generateImage")
+
 const client = new Discord.Client({
     intents: [
         "GUILDS",
-        "GUILD_MESSAGES"
+        "GUILD_MESSAGES",
+        "GUILD_MEMBERS"
     ]
 })
 client.on("ready", () => {
@@ -14,6 +18,16 @@ client.on("messageCreate", (message) => {
     if (message.content == "Hello World!"){
         message.reply("Hello World!")
     }
+})
+
+const welcomeChannelId = "947635791276961813"
+
+client.on("guildMemberAdd", async (member) => {
+    const img = await generateImage(member)
+    member.guild.channels.cache.get(welcomeChannelId).send({
+        content: `<@${member.id}> Welcome to the server!`,
+        files: [img]
+    })
 })
 
 client.login(process.env.TOKEN)
